3fc240fa6074225d5f2e2a3df0f11355a3068113
Sudhamsh Mondrati <smondra1@uncc.edu>
2022-10-20 19:45:29 -0400

    working prototype of search rating page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, Markup
 
 from src.repositories.movie_repository import get_movie_repository
 
 app = Flask(__name__)
 
 movie_repository = get_movie_repository()
-
+movie_repository.create_movie("Spider-Man: No Way Home (2021)", "Marvel", "7.4")
 
 @app.get('/')
 def index():
@@ -15,7 +15,8 @@ def index():
 @app.get('/movies')
 def list_all_movies():
     # TODO: Feature 1
-    return render_template('list_all_movies.html', list_movies_active=True)
+    print(movie_repository.get_all_movies()[0].title)
+    return render_template('list_all_movies.html', list_movies_active=True, movie_list=movie_repository)
 
 
 @app.get('/movies/new')
@@ -33,4 +34,13 @@ def create_movie():
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
+    movie_rating = ''
+    if request.args:
+        input_title = request.args.get('movie-name')
+        found_movie = movie_repository.get_movie_by_title(input_title)
+        if not found_movie:
+            return render_template('search_movies.html', search_active=True, movie_found=False)
+        movie_rating = found_movie.rating
+        print(movie_rating)
+        return render_template('search_movies.html', search_active=True, movie=found_movie, movie_found=True)
     return render_template('search_movies.html', search_active=True)
